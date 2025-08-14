ea76cd4924aef9eb383946b2eba1b7dc41964a6b
smmasudrana <smmasudrana62@gmail.com>
2025-08-14 11:19:58 +0600

    problem number 4 solved

---
+++ problem4.js
@@ -0,0 +1,20 @@
+function  isSame(arr1 , arr2 ) {
+    if(!Array.isArray(arr1) || !Array.isArray(arr2)){
+        return 'Invalid';
+    }
+
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] !== arr2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+const bal = isSame([1, 2, 3] , [1 , 2 , 3]);
+console.log(bal);
+
